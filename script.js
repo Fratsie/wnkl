a25b64a8c728f38c0677e981e0e9b2d864c362d7
Fratsie <118970868+Fratsie@users.noreply.github.com>
2024-10-13 21:57:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+var visibility = false;
+var categories = ['pouchbag', 'chipsEnZoutjes', 'koek', 'chocolade', 'baru poeders', 'pasta', 'pastasauzen', 'pesto', 'tapenade', 'hummus'];
+
+function change_visibility(){
+    if(visibility){
+        document.getElementById('productCategories').style.visibility = 'hidden'
+        visibility = false
+    }
+    else{
+        document.getElementById('productCategories').style.visibility = 'visible'
+        visibility = true
+    }
+}
+
+function product_categories(category){
+    container = document.getElementById('productContainer');
+    container.innerHTML = "";
+
+    fetch('data.json').then(data => data.json()).then(response => {
+        var allProducts = response;
+        var productsCategory = allProducts[category];
+
+        for(var i in productsCategory){
+            var product = productsCategory[i];
+
+            //declare product details
+            var productName = product['naam'];
+            var productVerkoopprijs = product['verkoopprijs'];
+            var productDescription = product['description'];
+            var productImage = product['image'];
+
+            //create elements
+            var div = document.createElement('div');
+            var img = document.createElement('img');
+            var h3 = document.createElement('h3');
+            var p = document.createElement('p');
+
+            //fill in elements
+            img.src = productImage;
+            img.alt = productName;
+            h3.innerHTML = productName + " - " + productVerkoopprijs;
+            p.innerHTML = productDescription;
+
+            //add everything to div
+            div.append(img);
+            div.append(h3);
+            div.append(p);
+
+            div.classList.add('product')
+
+            //append div to productscontainer
+            container.append(div);
+        }
+    });
+
+    change_visibility();
+}
+
+function search_products(){
+    event.preventDefault();
+
+    container = document.getElementById('productContainer');
+    container.innerHTML = "";
+
+    var productsArray = [];
+    var search_results = [];
+
+    fetch('data.json').then(data => data.json()).then(response => {
+        var allProducts = response;
+
+        categories.forEach(element => {
+            var productsCategory = allProducts[element];
+
+            for(var product in productsCategory){
+                productsArray.push(productsCategory[product]);
+            }
+        });
+
+        productsArray.forEach(element => {
+            var search_input = document.getElementById('searchbar').value.trim().toLowerCase();
+
+            if(element['naam'].includes(search_input) && search_input != ""){
+                search_results.push(element);
+            }
+        });
+
+        for(var i in search_results){
+            var product = search_results[i];
+
+            //declare product details
+            var productName = product['naam'];
+            var productVerkoopprijs = product['verkoopprijs'];
+            var productDescription = product['description'];
+            var productImage = product['image'];
+
+            //create elements
+            var div = document.createElement('div');
+            var img = document.createElement('img');
+            var h3 = document.createElement('h3');
+            var p = document.createElement('p');
+
+            //fill in elements
+            img.src = productImage;
+            img.alt = productName;
+            h3.innerHTML = productName + " - " + productVerkoopprijs;
+            p.innerHTML = productDescription;
+
+            //add everything to div
+            div.append(img);
+            div.append(h3);
+            div.append(p);
+
+            div.classList.add('product')
+
+            //append div to productscontainer
+            container.append(div);
+        }
+    });
+}
